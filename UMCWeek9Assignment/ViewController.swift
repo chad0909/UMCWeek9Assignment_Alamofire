a059dd3ff74737229f6175762a290ecf671064da
chad0909@naver.com <108044639+chad0909@users.noreply.github.com>
2022-11-23 09:43:32 +0900

    Initial Commit

---
+++ UMCWeek9Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UMCWeek9Assignment
+//
+//  Created by 077tech on 2022/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
